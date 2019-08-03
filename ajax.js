49e9945b01cf0648fe499c63d08d60847909066c
Marie Reutter <44366204+MarieRe@users.noreply.github.com>
2019-08-03 16:24:23 +0200

    update

--- ajax.js
+++ ajax.js
@@ -1,15 +1,59 @@
 
+$(function () {
+  var Domain = 'https://dhbw.cheekbyte.de/calendar/test';
+  URL = Domain + "/events";
+  var $events = $('#showEvents');
 
-var Domain = 'https://dhbw.cheekbyte.de/calendar/test';
-var Appointments = null;
+  var $Title = $('#title');
+  var $Location = $('#location');
+  var $Organizer = $('#organizer');
+  var $Start = $(document.getElementById(selected).innerHTML+'#start');
+  var $End = $(document.getElementById(selected).innerHTML+'#end');
+  var $Status = $('#status');
+  var $Allday = $('#allday');
+  var $Webpage = $('#webpage');
+  var $IMAGE = $('#imageurl');
 
-URL = Domain + "/events";
-var getAllEvents = $.ajax({
-  method: "GET",
-  url: URL
-});
+  var eventData = {
+    title: $Title.val(),
+    location: $Location.val(),
+    organizer: $Organizer.val(),
+    start: $Start.val(),
+    end: $End.val(),
+    status: $Status.val(),
+    allday: $Allday.val(),
+    webpage: $Webpage.val(),
+    imageurl: $IMAGE.val(),
+  }
 
+  $.ajax({
+    type: "GET",
+    url: URL,
+    success: function(events) {
+      $.each(events, function(i, event){
+        $events.append(`
+          <li><ul>
+            <li>Title: `+ event.title +`</li>
+            <li>Location: `+ event.location +`</li>
+            <li>Organizer: `+ event.organizer +`</li>
+            <li>Start: `+ event.start +`</li>
+            <li>End: `+ event.end +`</li>
+          </ul></li>
+        `)
+      });
+    }
+  });
 
+  $.ajax({
+    type: "POST",
+    url: URL,
+
+  })
+}
+)
+ 
+
+/*
 getAllEvents.done(function (done) {
   console.log(done);
 });
@@ -18,36 +62,33 @@ getAllEvents.fail(function (fail) {
   console.log(fail);
 });
 
-
-
 $ ("#eventForm").submit(function(eventData){
   alert("Handler for .submit() called.");
   eventData.preventDefault()
-  
-  var evtID = parseInt($('input[name="formID"]').val());
-  var evtTitle = $('input[name="formTitle"]').val();
-  var evtLocation = $('input[name="formLocation"]').val();
-  var evtOrganizer = $('input[name="formOrganizer"]').val();
-  var evtStart = $('input[name="formStart"]').val();
-  var evtEnd = $('input[name="formEnd"]').val();
-  var evtStatus = $('select[name="formStatus"]').val();
-  var evtAllday = $('input[name="formAllday"]').val();
-  var evtWebpage = $('input[name="formWeb"]').val();
-  var evtIMAGE = $('input[name="formImage"]').val();
+
+  var Title = $(Title"]')
+  var Location = $(Location"]')
+  var Organizer = $(Organizer"]')
+  var Start = $(Start"]')
+  var End = $(End"]')
+  var Status = $('select[name="formStatus"]')
+  var Allday = $(Allday"]')
+  var Webpage = $(Web"]')
+  var IMAGE = $(Image"]')
 
   
   
   
   var eventData = {
-    "title": evtTitle,
-    "location": evtLocation,
-    "organizer": evtOrganizer,
-    "start": evtStart,
-    "end": evtEnd,
-    "status": evtStatus,
-    "allday": evtAllday,
-    "webpage": evtWebpage,
-    "imageurl": evtIMAGE,
+    "title": Title,
+    "location": Location,
+    "organizer": Organizer,
+    "start": Start,
+    "end": End,
+    "status": Status,
+    "allday": Allday,
+    "webpage": Webpage,
+    "imageurl": IMAGE,
   }
   
   $.ajax({
@@ -66,3 +107,5 @@ $ ("#eventForm").submit(function(eventData){
     console.log(fail);
   });
 });
+*/
+
--- calendar.js
+++ calendar.js
@@ -1,8 +1,4 @@
-$(document).ready(function() {
-  $('#btnShowAll').click(function(){
-    listEventsSidebar();
-  });
-});
+var AllEventList = "";
 
 function updateView() {
   var month = document.getElementById("months").value;
@@ -28,7 +24,14 @@ function updateView() {
 
 function dayclick(day) {
   var month = document.getElementById("months").value;
+  if (month == "October" || month == "November" || month=="December") {
+    var monthnumber = document.getElementById("months").selectedIndex + 1;
+  } else {
+    var monthnumber = document.getElementById("months").selectedIndex + 1;
+    monthnumber = "0" + monthnumber;
+  }
   var year = document.getElementById("years").value;
+  document.getElementById("selected").innerHTML = year+"-"+monthnumber+"-"+day+T;
   var listel = document.getElementById("days").getElementsByTagName("LI");
   for (var i = 0; i < listel.length; i++) {
     listel[i].style.backgroundColor = "rgb(255,232,232)";
@@ -40,6 +43,7 @@ function dayclick(day) {
     <button id='changeback' onclick='changebackall()' class='fas fa-angle-left'></button>
     <button id='btnShowAll' class='far fa-eye'></button>
     <button id='addevent' onclick='addevent()' class='fas fa-calendar-plus'></button></div>`;
+  AllEventList = document.getElementById("changebody").innerHTML;
   document.getElementById("changebody").innerHTML = "";
 }
 
@@ -50,85 +54,31 @@ function changebackall() {
   }
   document.getElementById("chheading").innerHTML = "Future Events";
   document.getElementById("changebtns").innerHTML = "<button id='btnShowAll' class='fas fa-eye'></button>";
-  document.getElementById("changebody").innerHTML = "<ul id='allevents'><li>new Event 1</li></ul>"
+  document.getElementById("changebody").innerHTML = AllEventList;
 }
 
 function addevent() {
+  AllEventList = document.getElementById("changebody").innerHTML;
   document.getElementById("changebody").innerHTML = `
     <form id="eventForm">
     <ul id='neweventform'>
         <li>
-            <input name='formTitle' type='text' placeholder='Event Title' required class='eventinput'>
+            <input id='title' type='text' placeholder='Event Title' required class='eventinput'>
         </li>
         <li>
-            <input name='formLocation' type='text' placeholder='Location' required class='eventinput'>
+            <input id='location' type='text' placeholder='Location' required class='eventinput'>
         </li>
-        <li><input name='formOrganizer' type='text' placeholder='Organizer' required class='eventinput'></li>
+        <li><input id='organizer' type='text' placeholder='Organizer' required class='eventinput'></li>
         <li>Start time: <input id='formStart' type='time' class='eventinput' value='08:00'></li>
         <li>End time: <input id='formEnd' type='time' class='eventinput' value='09:00' 
-        </li> <li>All day event? <input name='formAllday' type='checkbox'></li>
-        <li><select name='formStatus' class='eventinput'>
+        </li> <li>All day event? <input id='allday' type='checkbox'></li>
+        <li><select id='status' class='eventinput'>
                 <option>Busy</option>
                 <option>Free</option>
             </select></li>
-        <li><input name='formWeb' type='text' placeholder='Webpage' class='eventinput'></li>
-        <li><input name='formImage' type='text' placeholder='Image-url' class='eventinput'></li>
+        <li><input id='webpage' type='text' placeholder='Webpage' class='eventinput'></li>
+        <li><input id='imageurl' type='text' placeholder='Image-url' class='eventinput'></li>
         <li><button id='formSubmit' class='eventinput' type='button'>Submit</button></li>
     </ul>
     </form>`;
-}
-
-function listEvents(){
-	if(Appointments != null)
-	{
-		for(var i = 0 ; i < Appointments.length ; i++)
-		{
-			if(Appointments[i] != null)
-			{
-				addEventToList(Appointments[i],i);
-			}
-		}
-	}
-	else{
-		console.log("no events stored");
-	}
-}
-
-function addEventToList(ev,iAppointments)
-{
-	// Check if element is already displayed
-	if($("#EP"+ev["id"]).length == 0)
-	{
-		
-		var img = "";
-		if(ev["imageurl"] != null)
-		{
-			img = "<img src='"+ev["imageurl"]+"'><br>";
-		}
-		
-		var cats = "";
-		for (i = 0; i < ev["categories"].length; i++) { 
-			cats += ev["categories"][i].name;
-			if(i != ev["categories"].length-1)
-			{
-				cats += " | ";
-			}
-		}
-		
-		
-		$("#allevents").append
-		('<div class="eventDetails" id="EP'+ev["id"]+'">'
-		+'<div title="DELETE EVENT" class="eventDetailsBtnDel" id="EV'+ev["id"]+'">&#x274C;</div>'
-		+'<div title="EDIT EVENT" class="eventDetailsBtnEdit" id="EV'+iAppointments+'">⚙️</div><br>'
-		+'<div style="text-align:center;margin-top:5px;margin-bottom:5px">'+cats+"</div>"
-		+ev["title"]+"<br>"
-		+ev["location"]+"<br>"
-		+ev["organizer"]+"<br>"
-		+ev["webpage"]+"<br>"
-		+ev["start"].substr(0,10)+" "+ev["start"].substr(11)+"<br>"
-		+ev["end"].substr(0,10)+" "+ev["end"].substr(11)+"<br>"
-		+ev["status"]+"<br>"
-		+img
-		+'</div>');
-	}
 }
